f90da88028d61c65f3734ad21c2affba82d91dcb
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-10-29 10:30:29 +0530

    Added some important route files

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get('/send' , (req , res) =>{
+    res.send("Send messages endpoint!!");
+})
+
+
+export default router;
