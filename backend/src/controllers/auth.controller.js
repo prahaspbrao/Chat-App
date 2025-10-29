f0d6363080b19c6fc8cfe242ff02e3cb0db99bd4
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-10-30 00:17:55 +0530

    .

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,19 @@ export const signup =  async (req , res) =>{
     const {fullName , email , password} = req.body;
 
     try {
-        
+        if(!fullName || !email || !password){
+            return res.status(400).json({message : "All fields are  required"})
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message : "Password should be atleast 6 charecters"})
+        }
+
+        // check if email valid : regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message : "Invalid email format"})
+        }
     } catch (error) {
         
     }
