36d8f43abfe7f284c0493664836e721a5ac3ed31
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-01 23:46:27 +0530

    Completed signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -38,8 +38,9 @@ export const signup =  async (req , res) =>{
         })
 
         if(newUser){
-            generateToken(newUser._id , res);
-            await newUser.save();
+
+            const savedUser = newUser.save();
+            generateToken(savedUser._id , res);
 
             res.status(201).json({
                 _id : newUser._id,
