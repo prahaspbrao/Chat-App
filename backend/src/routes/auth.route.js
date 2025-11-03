3cbe35af722e68042e1860efb9086431370ae84b
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-03 19:32:46 +0530

    Added some updations on router

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,13 +4,7 @@ import { login , signup , logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup" ,signup);
-
-router.get("", (req , res) =>{
-    res.send("Home Page");
-})
-
 router.post("/login" , login);
-
 router.post("/logout" , logout);
 
 export default router;
