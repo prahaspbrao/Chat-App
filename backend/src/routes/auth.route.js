c2aa28181f22e64cc392bfa75192ffa053320234
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-03 19:23:15 +0530

    Added controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import signup from "../controllers/auth.controller.js";
+import { login , signup , logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -9,8 +9,8 @@ router.get("", (req , res) =>{
     res.send("Home Page");
 })
 
-router.post("/login" , login)
+router.post("/login" , login);
 
-router.post("/logout" , logout)
+router.post("/logout" , logout);
 
 export default router;
