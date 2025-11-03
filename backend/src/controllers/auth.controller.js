9afecdf9c1498ce3f77b00126125258ced4985ab
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-03 19:24:42 +0530

    changed cookies to cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,6 +98,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (_ , res) => {
-    res.cookies("jwt" , "" , {maxAge : 0});
+    res.cookie("jwt" , "" , {maxAge : 0});
     res.status(200).json({message : "Logged out successfully"});
 }
