02049582f5df4f98a1b49bbafde1b80f9a885bd0
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-05 18:17:52 +0530

    Completed rate limiting

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,20 @@
+import { ENV } from "./env.js";
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+
+const aj = arcjet({
+  key: ENV.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      max: 100,        // reasonable default
+      interval: 60,    // per minute
+      identity: "ip",  // per IP rate limit
+    }),
+  ],
+});
+
+export default aj;
