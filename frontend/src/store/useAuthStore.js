9f97ff3f6e9a3981accc73b960df9ef75d80417b
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-06 20:17:32 +0530

    Start of signup functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,4 @@
 import {create} from "zustand";
 
 export const useAuthStore = create((set , _) => ({
-    authUser : {name : "Jhon" , _id : 123 , age : 25},
-    isLoading : false,
-    isLoggedIn : false,
-
-    login : () => {
-        console.log("We hjust logged In!!");
-        set({isLoggedIn : true});
-    }
 }))
