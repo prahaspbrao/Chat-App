89853921021e4d644da7928a9e9028fea1fa4ed7
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-10 14:24:51 +0530

    Trying to fix the bug again and agian

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import {axiosInstance} from "../lib/axios.js";
+import axiosInstance from "../lib/axios.js";
 import { toast } from "react-hot-toast";
 
 const useChatStore = create((set, get) => ({
