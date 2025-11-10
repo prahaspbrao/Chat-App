11454b5ed76a2df6889e05ad27f63ef82b102dbd
Prahas P B Rao <prahaspbrao2005@gmail.com>
2025-11-10 19:50:53 +0530

    Updated the rate limiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,21 @@
 import express from "express";
-import { login, signup, logout } from "../controllers/auth.controller.js";
+import { login, signup, logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middlewares.js";
-import { updateProfile } from "../controllers/auth.controller.js";
+import arcjetProtection  from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+// ✅ Arcjet ONLY for signup/login (high-risk endpoints)
+router.post("/signup", arcjetProtection, signup);
+router.post("/login", arcjetProtection, login);
+
 router.post("/logout", logout);
 
+// ✅ update profile protected by auth ONLY
 router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, (req, res ) => {
+// ✅ auth check - protected route
+router.get("/check", protectRoute, (req, res) => {
   res.status(200).json(req.user);
 });
 
