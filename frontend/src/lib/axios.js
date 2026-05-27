4ab96dde17bfe1b3d31fe38d02686b192de0ab85
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-05-27 21:29:30 +0530

    Updated front end

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "http://chat-app-e3t1.vercel.app//api",
   withCredentials: true,
 });
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axiosInstance from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {connect, io} from "socket.io-client"
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "http://chat-app-e3t1.vercel.app";
 
 export const useAuthStore = create((set , get) => ({
   authUser: null,
