d544b8c12c18b75ce7ff96f5a38aef2c2859c95f
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-05-27 21:14:05 +0530

    Updated to localhost

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axiosInstance from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {connect, io} from "socket.io-client"
-const BASE_URL = import.meta.env.MODE === "development" ? "localhost:3000/" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
 
 export const useAuthStore = create((set , get) => ({
   authUser: null,
