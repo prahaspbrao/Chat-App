8110fa8fa8bd908a9fb43a77fee0f2971b4aef30
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-05-27 22:04:55 +0530

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://localhost:3000/api" : "https://chat-app-e3t1.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://localhost:3000/api" : "https://chat-app-backend-9j4g.onrender.com/api",
   withCredentials: true,
 });
 
