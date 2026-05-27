9ceb60570b122f57f141e8e84f79a878a9953893
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-05-27 20:59:50 +0530

    Updated commit

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axiosInstance from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {connect, io} from "socket.io-client"
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chat-n9aefej1r-prahas-p-b-raos-projects.vercel.app" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "localhost:3000/" : "/";
 
 export const useAuthStore = create((set , get) => ({
   authUser: null,
